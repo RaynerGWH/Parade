31d2966b06f46bc7090645ac22765748ebecaac5
RaynerGWH <raynergwh@gmail.com>
2025-03-15 18:16:02 +0800

    changed card comparator to make it cleaner

--- src/cards/CardComparator.java
+++ src/cards/CardComparator.java
@@ -1,75 +1,32 @@
 import java.util.Comparator;
 
 public class CardComparator implements Comparator<Card> {
-
-    public CardComparator(){};
     
     //Compare method. sorts them by color first(purple->blue->red->orange->grey->green), then face value(ascending).
     @Override
     public int compare(Card c1, Card c2) {
-        int res = 0;
-        int c1Score = 0;
-        int c2Score = 0;
 
-        // TODO: Combine switch statements
-        switch (c1.getColor()) {
-            case PURPLE:
-                c1Score = 1;
-                break;
+        int colorDifference = 0;
 
-            case BLUE:
-                c1Score = 2;
-                break;
+        colorDifference = getColorScore(c1.getColor()) - getColorScore(c2.getColor());
 
-            case RED:
-                c1Score = 3;
-                break;
-
-            case ORANGE:
-                c1Score = 4;
-                break;
-
-            case GREY:
-                c1Score = 5;
-                break;
-
-            case GREEN:
-                c1Score = 6;
-                break;
+        if (colorDifference != 0) {
+            return colorDifference;
         }
 
-        switch (c2.getColor()) {
-            case PURPLE:
-                c2Score = 1;
-                break;
+        // If the two card's colors are the same, we will then compare the values of the two cards.
+        return c1.getValue() - c2.getValue();
+    }
 
-            case BLUE:
-                c2Score = 2;
-                break;
-
-            case RED:
-                c2Score = 3;
-                break;
-
-            case ORANGE:
-                c2Score = 4;
-                break;
-
-            case GREY:
-                c2Score = 5;
-                break;
-
-            case GREEN:
-                c2Score = 6;
-                break;
+    private int getColorScore(Color color) {
+        switch (color) {
+            case PURPLE: return 1;
+            case BLUE: return 2;
+            case RED: return 3;
+            case ORANGE: return 4;
+            case GREY: return 5;
+            case GREEN: return 6;
+            default: throw new IllegalArgumentException("Unexpected color: " + color);
         }
-
-        res = c1Score - c2Score;
-        if (res != 0) {
-            return res;
-        }
-
-        res = c1.getValue() - c2.getValue();
-        return res;
     }
 }
